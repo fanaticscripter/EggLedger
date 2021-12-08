8f41a9c770ccad33cd690556178ff5bec88643d6
fanaticscripter <fanaticscripter@gmail.com>
2021-12-08 14:11:19 +0800

    build-macos.sh: forget about universal binary
    The arm64 build doesn't work for whatever reason. We weren't using cgo before
    so sqlite doesn't work. Once CGO_ENABLED is turned on, during the build there
    are a lot of warnings dumped:
    
        ld: warning: building for iOS, but linking in .tbd
        file (/Library/Developer/CommandLineTools/SDKs/MacOSX11.sdk/System/Library/Frameworks//CoreFoundation.framework/CoreFoundation.tbd)
        built for macOS/Mac Catalyst
    
    And once deployed to target machine that target machine simply can't execute
    the binary.
    
    Let's just rely on Rosetta instead.

--- build-macos.sh
+++ build-macos.sh
@@ -4,16 +4,7 @@ version=$(<VERSION)
 echo "generating $app v${version}..."
 rm -rf $app
 mkdir -p $app/Contents/{MacOS,Resources}
-amd64_bin=$app/Contents/MacOS/EggLedger_amd64
-arm64_bin=$app/Contents/MacOS/EggLedger_arm64
-universal_bin=$app/Contents/MacOS/EggLedger
-echo "generating darwin amd64 build..."
-GOOS=darwin GOARCH=amd64 go build -o $amd64_bin
-echo "generating darwin arm64 build..."
-GOOS=darwin GOARCH=arm64 go build -o $arm64_bin
-echo "generating universal binary..."
-lipo -create $amd64_bin $arm64_bin -output $universal_bin
-rm $amd64_bin $arm64_bin
+GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build -o $app/Contents/MacOS/EggLedger
 cat > $app/Contents/Info.plist <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
